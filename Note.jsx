2eb23e532e7e2395c2a859688de0f66f6993c936
Abhishek Choudhary <80909702+Abhishek48784@users.noreply.github.com>
2021-06-05 15:48:32 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        Hi,my name is Abhishek. today is the last session of this bootcamp and i
+        wanna say that this is really an amazing bootcamp and i really enjoyed
+        it.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
